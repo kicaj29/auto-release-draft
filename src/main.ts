e898b26eceb122e8469d6fd3fffafc9f562f4c91
Jacek Kowalski <kicaj29@wp.pl>
2021-05-10 16:43:53 +0200

    maybe ready to use action

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,22 @@ import * as core from '@actions/core'
 import * as event from './event';
 import * as version from './version';
 import * as git from './git';
+import * as github from './github';
 
 export async function run(): Promise<void> {
   try {
+    const token = core.getInput('repo-token');
+    let releaseUrl = '';
+
     const tag = event.getCreatedTag();
 
     if (tag && version.isSemVer(tag)) {
       const changelog = await git.getChangesIntroducedByTag(tag);
+
+      releaseUrl = await github.createReleaseDraft(tag, token, changelog);
     }
 
-    core.setOutput('release-url', 'https://example.com');
+    core.setOutput('release-url', releaseUrl);
   } catch (error) {
     // set exit code of the action to something different then zero and print error message to the build log:
     core.setFailed(error.message)
