9e79f2914b1fd4b45222d48befa09cc4b6951896
Jacek Kowalski <kicaj29@wp.pl>
2021-05-10 14:20:37 +0200

    basic version of the action

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,10 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 
-async function run(): Promise<void> {
+export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('release-url', 'https://example.com');
   } catch (error) {
+    // set exit code of the action to something different then zero and print error message to the build log:
     core.setFailed(error.message)
   }
 }
