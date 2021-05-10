163d120c09bc33c82f2f43c2cdc49517d0170a51
Jacek Kowalski <kicaj29@wp.pl>
2021-05-10 16:23:13 +0200

    git operations

---
+++ src/git.ts
@@ -0,0 +1,97 @@
+import {exec, ExecOptions} from '@actions/exec';
+import * as core from '@actions/core'
+
+export async function getChangesIntroducedByTag(tag: string): Promise<string> {
+
+    const previousVersionTag = await getPreviousVersionTag(tag);
+
+    return previousVersionTag ? getCommitMessagesBetween(previousVersionTag, tag)
+        : getCommitMessagesFrom(tag);
+}
+
+export async function getPreviousVersionTag(tag: string): Promise<string | null> {
+
+    let previousTag = '';
+
+    const options: ExecOptions = {
+        listeners: {
+            stdout: (data: Buffer) => {
+                previousTag += data.toString();
+            },            
+        },
+        silent: true, // do not log executed operations to the output
+        ignoreReturnCode: true
+    }
+    
+    const exitCode = await exec(
+        'git', // name of command
+        [
+            'describe',
+            '--match', 'v[0-9]*',
+            '--abbrev=0',       // print only tag name
+            '--first-parent',   // only search the current branch in case of merge commit
+            `${tag}^`           // start looking from the parent of the specified tag
+        ],
+        options
+    );
+
+    core.debug(`The previous version tag us ${previousTag}`);
+
+    return exitCode === 0 ? previousTag.trim() : null;
+}
+
+export async function getCommitMessagesBetween(firstTag: string, secondTag: string): Promise<string> {
+
+    let commitMessages = '';
+
+    const options: ExecOptions = {
+        listeners: {
+            stdout: (data: Buffer) => {
+                commitMessages += data.toString();
+            },            
+        },
+        silent: true, // do not log executed operations to the output
+    }
+    
+    const exitCode = await exec(
+        'git', // name of command
+        [
+            'log',
+            '--format=%s',  //print only first line of the commit message
+            `${firstTag}..${secondTag}` //return only the commits which are reachable from the second tag but not from the first tag
+        ],
+        options
+    );  //we do not check exit code because git log does not fail
+
+    core.debug(`The commit messages between ${firstTag} and ${secondTag} are: ${commitMessages}.` );
+
+    return commitMessages.trim();
+}
+
+export async function getCommitMessagesFrom(tag: string): Promise<string> {
+
+    let commitMessages = '';
+
+    const options: ExecOptions = {
+        listeners: {
+            stdout: (data: Buffer) => {
+                commitMessages += data.toString();
+            },            
+        },
+        silent: true, // do not log executed operations to the output
+    }
+    
+    const exitCode = await exec(
+        'git', // name of command
+        [
+            'log',
+            '--format=%s',  //print only first line of the commit message
+            tag
+        ],
+        options
+    );  //we do not check exit code because git log does not fail
+
+    core.debug(`The commit messages from ${tag} are: ${commitMessages}.` );
+
+    return commitMessages.trim();
+}
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 import * as core from '@actions/core'
 import * as event from './event';
 import * as version from './version';
+import * as git from './git';
 
 export async function run(): Promise<void> {
   try {
     const tag = event.getCreatedTag();
 
-    /*if (tag && version.isSemVer(tag)) {
-
-    }*/
+    if (tag && version.isSemVer(tag)) {
+      const changelog = await git.getChangesIntroducedByTag(tag);
+    }
 
     core.setOutput('release-url', 'https://example.com');
   } catch (error) {
